1f7b788f9af6e8f183a426d079f1d5745df64be0
jiggacollins <120271879+jiggacollins@users.noreply.github.com>
2023-11-18 21:49:15 +0100

    Update nexus-installation6.sh

--- nexus-installation/nexus-installation6.sh
+++ nexus-installation/nexus-installation6.sh
@@ -23,10 +23,10 @@ sudo yum install java-11-openjdk-devel java-1.8.0-openjdk-devel -y
 
 # 2. Download nexus software and extract it (unzip)
 
-sudo wget http://download.sonatype.com/nexus/3/nexus-3.15.2-01-unix.tar.gz 
+sudo wget http://download.sonatype.com/nexus/3/nexus-3.47.1-01-unix.tar.gz 
 
-sudo tar -zxvf nexus-3.15.2-01-unix.tar.gz
-mv /opt/nexus-3.15.2-01 /opt/nexus
+sudo tar -zxvf nexus-3.47.1-01-unix.tar.gz
+mv /opt/nexus-3.47.1-01 /opt/nexus
 
 
 #5 Change the owner and group permissions to /opt/nexus and /opt/sonatype-work directories.
